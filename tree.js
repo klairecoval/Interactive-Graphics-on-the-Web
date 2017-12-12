618c8d4a089634dcc9ba1103fb7d0b9e5e151b7c
Claire Koval <klairecoval@gmail.com>
2017-12-12 17:48:40 -0500

    Added mouseover and mouseout event handlers for color change

--- tree.js
+++ tree.js
@@ -1,25 +1,25 @@
 //setup drawing surface
 var s = Snap(1100, 730);
 
-//background 
+//make blue sky background
 var sky = s.rect(0, 0, 1100, 730);
 sky.attr({
 	fill:"#79A3CC"
 })
 
-//grass
+//make rectangle for grass, green
 var grass = s.rect(0, 700, 1100, 30);
 grass.attr({
 	fill: "#008000"
 })
 
-//make trunk
+//make tree trunk
 var trunk = s.rect(460, 300, 140, 420);
 trunk.attr({
 	fill: "#372020"
 })
 
-//curve trunk
+//create ellipses to curve the trunk
 var curve = s.ellipse(460, 400, 30, 300);
 var curve2 = s.ellipse(600, 400, 30, 300);
 var curvetrunk = s.group(curve, curve2);
@@ -27,6 +27,14 @@ curvetrunk.attr({
     fill:"#79A3CC"
 })
 
+//make a basket that could hold apples but really just looks cute
+var basket = s.polygon(300, 720, 280, 670, 360, 670, 340, 720);
+basket.attr({
+	fill:"#d7c18c"
+})
+
+
+//TREE\\
 //create circles for tree leaves
 var circ1 = s.circle(500, 260, 80);
 var circ2 = s.circle(480, 240, 80);
@@ -37,14 +45,13 @@ var circ6 = s.circle(570, 240, 80);
 var circ7 = s.circle(590, 220, 80);
 var circ8 = s.circle(580, 260, 80);
 var circ9 = s.circle(600, 240, 80);
-
-//group leaves and make green
+//group leaves/circles and make green
 var leaves = s.group(circ1, circ2, circ3, circ4, circ5, circ6, circ7, circ8, circ9);
 leaves.attr({
 	fill: "#004C00"
 })
 
-//apples
+//create apples and group to make red
 var apple1 = s.ellipse(450, 240, 15, 17);
 var apple2 = s.ellipse(560, 290, 15, 17);
 var apple3 = s.ellipse(530, 180, 15, 17);
@@ -57,13 +64,15 @@ orchard.attr({
 	fill: "#C02F2F"
 })
 
+//APPLE DRAGGABILITY\\
+//move the apple depending on x and y coordinate of dragged mouse
 var move = function(dx,dy) {
-        this.attr({
-                    transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy],
-			
-                });
+	this.attr({
+		transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy],
+	});
 }
 
+//transform the apple to its new location
 var start = function() {
     apple1.data('origTransform', this.transform().local );
 	apple2.data('origTransform', this.transform().local );
@@ -73,10 +82,13 @@ var start = function() {
 	apple6.data('origTransform', this.transform().local );
 	apple7.data('origTransform', this.transform().local );
 }
+
+//let me know apple has been released
 var stop = function() {
         console.log('finished dragging');
 }
 
+//add functions to all apples
 apple1.drag(move, start, stop );
 apple2.drag(move, start, stop );
 apple3.drag(move, start, stop );
@@ -84,3 +96,112 @@ apple4.drag(move, start, stop );
 apple5.drag(move, start, stop );
 apple6.drag(move, start, stop );
 apple7.drag(move, start, stop );
+
+//INTERACTIVITY\\
+//toggle the color of the apples when moused over to indicate interactivity
+apple1.mouseover(
+	function(){
+		apple1.attr({fill:"#C16666"})
+	}
+)
+apple1.mouseout(
+	function(){
+		apple1.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple2.mouseover(
+	function(){
+		apple2.attr({fill:"#C16666"})
+	}
+)
+apple2.mouseout(
+	function(){
+		apple2.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple3.mouseover(
+	function(){
+		apple3.attr({fill:"#C16666"})
+	}
+)
+apple3.mouseout(
+	function(){
+		apple3.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple4.mouseover(
+	function(){
+		apple4.attr({fill:"#C16666"})
+	}
+)
+apple4.mouseout(
+	function(){
+		apple4.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple5.mouseover(
+	function(){
+		apple5.attr({fill:"#C16666"})
+	}
+)
+apple5.mouseout(
+	function(){
+		apple5.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple6.mouseover(
+	function(){
+		apple6.attr({fill:"#C16666"})
+	}
+)
+apple6.mouseout(
+	function(){
+		apple6.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the stem when moused over to indicate interactivity
+apple7.mouseover(
+	function(){
+		apple7.attr({fill:"#C16666"})
+	}
+)
+apple7.mouseout(
+	function(){
+		apple7.attr({fill:"#C02F2F"})
+	}
+)
+
+//toggle the color of the trunk when moused over to indicate interactivity
+trunk.mouseover(
+	function(){
+		trunk.attr({fill:"#6D492B"})
+	}
+)
+trunk.mouseout(
+	function(){
+		trunk.attr({fill:"#372020"})
+	}
+)
+
+//move the apples back to their original places on the tree after being dragged away
+var upforplucking = function(){
+    apple1.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+    apple2.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+    apple3.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+    apple4.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+    apple5.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+	apple6.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+	apple7.animate({transform: 'translate(0, 0)'}, 1500, mina.easein);
+}
+trunk.click(upforplucking); //add function to event handler
